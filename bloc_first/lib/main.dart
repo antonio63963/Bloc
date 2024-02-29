b144823e027bdff0e3e70b78dde864bae4b28ca8
Anton <shotvideo25@gmail.com>
2024-02-29 16:03:19 +0200

    first bloc

---
+++ bloc_first/lib/main.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:bloc_first/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlocProvider(
+        create: (_) => PersonsBloc(),
+        child: const MyHomePage(title: 'Flutter Bloc First'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final Bloc bloc;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    context
+                        .read<PersonsBloc>()
+                        .add(const LoadPersonsAction(url: PersonUrl.persons1));
+                  },
+                  child: const Text("JSON 1")),
+              const SizedBox(width: 24),
+              ElevatedButton(
+                  onPressed: () {
+                    context
+                        .read<PersonsBloc>()
+                        .add(const LoadPersonsAction(url: PersonUrl.persons2));
+                  },
+                  child: const Text("JSON 2")),
+            ],
+          ),
+          BlocBuilder<PersonsBloc, FetchResult?>(
+            buildWhen: (previousResultState, currentResultState) =>
+                previousResultState?.persons != currentResultState?.persons,
+            builder: (context, fetchResultState) {
+              final persons = fetchResultState?.persons;
+              fetchResultState?.log();
+              if (persons == null) {
+                return const SizedBox();
+              } else {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: persons.length,
+                  itemBuilder: (_, idx) {
+                    final pers = persons[idx]!;
+                    return ListTile(
+                      title: Text(pers.name),
+                      subtitle: Text(pers.age.toString()),
+                    );
+                  },
+                );
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ cubit_test/lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
+
+const names = ['Foo', 'Bar', 'Baz'];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() {
+    final timer = Timer(Duration(seconds: 2), () {
+      emit(names.getRandomElement());
+    });
+    timer.cancel();
+  }
+
+  @override
+  String toString() {
+    super.toString();
+    return '''
+      pickRandomName: $pickRandomName,
+    ''';
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+    print(cubit);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cubit.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: StreamBuilder<String?>(
+          stream: cubit.stream,
+          builder: (context, snapshot) {
+            final button = TextButton(
+              onPressed: () => cubit.pickRandomName(),
+              child: const Text('Pick a random name'),
+            );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return button;
+              case ConnectionState.waiting:
+                return button;
+              case ConnectionState.active:
+                return Column(
+                  children: [
+                    Text(snapshot.data ?? ''),
+                    button,
+                  ],
+                );
+              case ConnectionState.done:
+                return const SizedBox();
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
